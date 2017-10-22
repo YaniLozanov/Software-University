9634d6add3ad0175f04c28779d08acb079e1cb20
Talasam <kronos1997@abv.bg>
2017-10-22 17:17:50 +0200

    Exercises

---
+++ Programming Fundamentals - January 2017/Data Types and Variables/Variable in Hexadecimal Format/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable_in_Hexadecimal_Format
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberInHexadecimal = Console.ReadLine();
+
+            int numberInDecimal = Convert.ToInt32(numberInHexadecimal,16);
+
+            Console.WriteLine(numberInDecimal);
+
+
+        }
+    }
+}
