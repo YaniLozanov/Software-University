f76a9cb143655ba3ef9718b631f79485f305c40e
Talasam <kronos1997@abv.bg>
2017-10-22 17:52:45 +0200

    Exercises

---
+++ Programming Fundamentals - January 2017/Data Types and Variables/Boolean Variable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string intputString = Console.ReadLine();
+
+            bool outputInBoolean = Convert.ToBoolean(intputString);
+
+            if (outputInBoolean)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if (!outputInBoolean)
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals - January 2017/Data Types and Variables/Employee Data/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fistName = "Amanda";
+            string lastName = "Jonson";
+            short age = 27;
+            char gender = 'f';
+            long personalIdNumber = 8306112507L;
+            int numOfEmployee = 27563571;
+
+            Console.WriteLine($"First name: {fistName}");
+            Console.WriteLine($"Last name: {lastName}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Gender: {gender}");
+            Console.WriteLine($"Personal ID: {personalIdNumber}");
+            Console.WriteLine($"Unique Employee number: {numOfEmployee}");
+
+
+
+        }
+    }
+}
