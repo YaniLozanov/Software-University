1e2c2ba78f60e769ee206b4d4a9d46156aacc97c
YaniLozanov <kronos1997@abv.bg>
2018-03-23 16:40:44 +0100

    Test

---
+++ Programming Fundamentals - януари 2018/05. Arrays - Exercises/Reverse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstLine = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int[] sum = new int[firstLine.Length];
+
+
+            for (int i = 0; i < firstLine.Length; i++)
+            {
+                sum[firstLine.Length - i - 1] = firstLine[i];
+            }
+
+            Console.WriteLine(string.Join(" ",sum));
+        }
+    }
+}
